b1ec5e53371a4c32123b967d65b82d992cdc4662
ben-barnard <34529890+ben-barnard@users.noreply.github.com>
2019-12-06 12:58:14 -0600

    Added LED indicator and Reset
    After testing, it appears that my memory management isn't good enough and the ESP32 is crashing after a day or so. While I will work to clean up my string issues, the quick and easy fix is to force a reboot (ESP.restart();) after each scan.

--- BLE_scan_GB2.ino
+++ BLE_scan_GB2.ino
@@ -3,6 +3,8 @@
    Ported to Arduino ESP32 by Evandro Copercini
 */
 
+int ledPin = 16;
+
 char* ssid_retrieve(){
   uint64_t chipid = ESP.getEfuseMac(); // The chip ID is essentially its MAC address(length: 6 bytes).
   uint16_t chip = (uint16_t)(chipid >> 32);
@@ -34,7 +36,7 @@ RTClib RTC;
 #include <BLEAdvertisedDevice.h>
 
 //Beacon List
-const int NUMBER_OF_ELEMENTS = 8;
+const int NUMBER_OF_ELEMENTS = 9;
 const int MAX_SIZE = 18;
 char beacons [NUMBER_OF_ELEMENTS] [MAX_SIZE] = {
  { "d0:fa:94:da:23:64" }, //Beacon #1
@@ -45,8 +47,9 @@ char beacons [NUMBER_OF_ELEMENTS] [MAX_SIZE] = {
  { "d9:00:a5:f0:e4:7d" }, //Beacon #6
  { "e4:fd:3c:56:87:99" }, //Beacon #7
  { "c5:20:67:1c:21:b7" }, //Beacon #8
+ { "eb:ce:9a:73:50:a0" }, //Beacon #9
  };
- 
+
 bool mac_Check(const char test_mac[]) {
   for (int i = 0; i < NUMBER_OF_ELEMENTS; i++) {//Checking to see if the Bluetooth Device is one of ours
         if (strcmp(test_mac, beacons[i]) == 0){return true;} //Match! 
@@ -59,23 +62,12 @@ BLEScan* pBLEScan;
 
 class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
     void onResult(BLEAdvertisedDevice advertisedDevice) {
-      //Serial.printf("Advertised Device: %s \n", advertisedDevice.getAddress().toString().c_str());
       if(mac_Check(advertisedDevice.getAddress().toString().c_str())) //this matches our list of addresses, let's log it
       {
-      //if (!SD.begin(CS, MOSI, MISO, SCK)) {
-      //        Serial.println("initialization failed!");
-      //        while(1);
-      //        }
       SD.begin(CS, MOSI, MISO, SCK);
-      //root = SD.open("/");
       root = SD.open("logging.csv", FILE_WRITE);
-      //strcat(ssid_retrieve(),".csv")
-      //char filename = strcat(ssid_retrieve(),".csv")
-      //if (!SD.exists(filename)){}
-      //root = SD.open(filename), FILE_WRITE);
       Serial.print(advertisedDevice.getAddress().toString().c_str());
       Serial.println(" matches our list of addresses, logging it");
-      //Serial.println(ssid_retrieve());
       root.print(ssid_retrieve());
       root.print(", ");
       root.print(advertisedDevice.getAddress().toString().c_str());
@@ -83,8 +75,6 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
       root.print(advertisedDevice.getRSSI());
       root.print(", "); 
       DateTime now = RTC.now();
-      //root.print(now.year(), DEC);
-      //root.print('/');
       root.print(now.month(), DEC);
       root.print('/');
       root.print(now.day(), DEC);
@@ -106,6 +96,7 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
       root.flush();
      /* close the file */
       root.close();
+      digitalWrite(ledPin, HIGH);
       }
     }
 };
@@ -113,24 +104,24 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
 
 
 void setup() {
+  pinMode(ledPin, OUTPUT);
   Wire.begin();
   Serial.begin(115200);
   Serial.println("Scanning...");
-  //Serial.println(strcat(ssid_retrieve(),".csv");
   BLEDevice::init("");
   pBLEScan = BLEDevice::getScan(); //create new scan
   pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
   pBLEScan->setActiveScan(true); //active scan uses more power, but get results faster
   pBLEScan->setInterval(100);
   pBLEScan->setWindow(99);  // less or equal setInterval value
+  BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
+  pBLEScan->clearResults();   // delete results fromBLEScan buffer to release memory
+  Serial.println("Restarting in 15 seconds!");
+  delay(15000);
+  digitalWrite(ledPin, LOW);
+  ESP.restart();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
-  Serial.print("Devices found: ");
-  Serial.println(foundDevices.getCount());
-  Serial.println("Scan done!");
-  pBLEScan->clearResults();   // delete results fromBLEScan buffer to release memory
-  delay(10000);
+  // This has been removed because there is no loop. One scan, then reboot.
 }
